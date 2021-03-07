8acb10ae61d1a9965b480366be42058d2b8019d2
tleza456 <78993146+tleza456@users.noreply.github.com>
2021-03-07 14:37:29 +0700

    Initial commit

---
+++ lib/Screen/gallery_screen.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class GalleryScreen extends StatefulWidget {
+  GalleryScreen({Key key}) : super(key: key);
+
+  @override
+  _GalleryScreenState createState() => _GalleryScreenState();
+}
+
+class _GalleryScreenState extends State<GalleryScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+       child: Scaffold(
+      body: Center(
+        child: Text("Gallery Page"),),
+       )
+    );
+  }
+}
---
+++ lib/Screen/home_screen.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:my_profile/Screen/gallery_screen.dart';
+import 'package:my_profile/Screen/profile_screen.dart';
+
+class HomeScreen extends StatefulWidget {
+  HomeScreen({Key key}) : super(key: key);
+
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  int _currentIndex = 0;
+  var pages = [
+    ProfileScreen(),
+    GalleryScreen(),
+  ];
+
+  _setCurrentIndex(int index){
+    setState((){
+      _currentIndex = index;
+    });
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+       child: Scaffold(
+      
+      body: pages[_currentIndex],
+        bottomNavigationBar: BottomNavigationBar(
+          onTap: _setCurrentIndex,
+          items: [
+            BottomNavigationBarItem(icon: Icon(Icons.people), label:"profile"),
+            BottomNavigationBarItem(icon: Icon(Icons.photo_album), label:"gallery"),
+          ],
+        ),
+       )
+    );
+  }
+}
